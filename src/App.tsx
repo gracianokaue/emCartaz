c0df0e34ba2d146483a6212f3a3c95cd9188a183
Kaue Graciano <kauegraciano93@gmail.com>
2023-02-01 22:12:23 -0300

    Iniciado o header

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styles from './App.module.css';
+
+const App = ()=> {
+
+
+  return( 
+
+  <div className={styles.main}> 
+    <header>
+      <div className={styles.containerHeader}>
+        
+      </div>
+    </header>
+  </div>
+);
+}
+
+export default App;
